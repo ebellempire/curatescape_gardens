17e05daad801ff99cb69d884019e64e602c43c44
Brian Cordyack <brian@cordyack.com>
2017-08-22 10:37:12 -0400

    added google play badge, updated app links, updated global nav, adjusted styles, updated contribution form texta

--- si_gardens/common/footer.php
+++ si_gardens/common/footer.php
@@ -24,7 +24,8 @@
 		<span id="copyright">Copyright &copy; <?php echo date('Y').' '.mh_owner_link();?></span> 
 		<br>
 		<span id="app-store-links"><?php mh_appstore_footer(); ?></span>
-		<a href="http://s.si.edu/communityofgardensapp" target="_blank"><img src="<?php echo img('Download_on_the_App_Store_Badge_US-UK_135x40.svg'); ?>" alt="Download on the app store"></a>
+		<a href="http://s.si.edu/communityofgardensapple" target="_blank"><img src="<?php echo img('Download_on_the_App_Store_Badge_US-UK_135x40.svg'); ?>" alt="Download on the app store" class="apple-badge"></a>
+		<a href="http://s.si.edu/communityofgardensandroid" target="_blank"><img src="<?php echo img('google-play-badge.png'); ?>" alt="Get it on Google Play" class="google-badge"></a>
 	</p>
 	
 	<div class="custom">
